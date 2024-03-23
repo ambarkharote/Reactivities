b6c4fc3f6d5eaa5e6323f49081f1da775c5fc9dc
ambarkharote <ambarkharote@gmail.com>
2024-03-23 13:41:08 +0530

    Added Mobx functionality

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,13 @@
 import { Button, Card, CardContent, CardDescription, CardHeader, CardMeta, Image } from "semantic-ui-react";
-import { Activity } from "../../../app/layout/models/activity";
-
-interface Props {
-    activity : Activity;
-    cancelSelectActivity: () => void;
-    openForm : (id: string) => void;
-}
+import { useStore } from "../../../app/stores/store";
 
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
+export default function ActivityDetails() {
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+
+    if(!activity) return;
+
     return(
         <Card fluid>
         <Image src={`/assets/categoryImages/${activity.category}.jpg`}  />
@@ -24,7 +23,7 @@ export default function ActivityDetails({activity, cancelSelectActivity, openFor
         <CardContent extra>
           <Button.Group widths='2'>
             <Button onClick={() => openForm(activity.id)} basic color="blue" content='Edit'></Button>
-            <Button onClick={cancelSelectActivity} basic color="grey" content='Cancel'></Button>
+            <Button onClick={cancelSelectedActivity} basic color="grey" content='Cancel'></Button>
           </Button.Group>
         </CardContent>
       </Card>
