debf7708efaf715f5ac9aa041c7554f6820b4900
ambarkharote <ambarkharote@gmail.com>
2024-06-11 01:24:04 +0530

    updated code

--- client-app/src/features/Profiles/ProfileHeader.tsx
+++ client-app/src/features/Profiles/ProfileHeader.tsx
@@ -1,14 +1,20 @@
 import { Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic } from "semantic-ui-react";
+import { Profile } from "../../app/models/Profile";
+import { observer } from "mobx-react-lite";
 
-export default function ProfileHeader() {
+interface Props {
+    profile: Profile;
+}
+
+export default observer(function ProfileHeader( {profile}: Props) {
     return (
         <Segment>
             <Grid>
                 <Grid.Column width={12}>
                     <Item.Group>
-                        <Item.Image avatar size="small" src={'/assets/user.png'} />
+                        <Item.Image avatar size="small" src={profile.image || '/assets/user.png'} />
                         <Item.Content verticalAlign="middle">
-                            <Header as='h1' content='Displayname' />
+                            <Header as='h1' content={profile.displayName} />
                         </Item.Content>
                     </Item.Group>
                 </Grid.Column>
@@ -34,4 +40,4 @@ export default function ProfileHeader() {
             </Grid>
         </Segment>
     )
-}
+})
--- client-app/src/features/Profiles/ProfilePage.tsx
+++ client-app/src/features/Profiles/ProfilePage.tsx
@@ -1,15 +1,32 @@
 import { Grid } from "semantic-ui-react";
 import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
+import { useStore } from "../../app/stores/store";
+import { useParams } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
-export default function ProfilePage() 
-{
+
+export default observer(function ProfilePage() {
+    const {username} = useParams<{username: string}>();
+    const {profileStore} = useStore();
+    const {loadingProfile, loadProfile, profile} = profileStore;
+
+    useEffect(() => {
+        loadProfile(username!);
+    }, [loadProfile, username])
+
+    if(loadingProfile) return <LoadingComponent content="Loading profile..." />
+
     return (
         <Grid>
             <Grid.Column width={16}>
-                <ProfileHeader />
-                <ProfileContent />
+                <>
+                <ProfileHeader profile={profile!} />
+                <ProfileContent profile={profile!} />
+                </>
             </Grid.Column>
         </Grid>
     )
-}
+})
